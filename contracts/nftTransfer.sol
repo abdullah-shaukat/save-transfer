ad08c13a56686ec7ab689055c0dd7d334b0e2838
Muhammah Abdullah (BlockChain ASE) <abdullah.shaukat@devsinc.com>
2023-01-24 17:00:05 +0500

    Updated API GET in contract

--- contracts/nftTransfer.sol
+++ contracts/nftTransfer.sol
@@ -1,11 +1,13 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.7;
 
 import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-contract NFTMarket is ChainlinkClient {
+contract NFTMarket is ChainlinkClient, ConfirmedOwner {
     using Chainlink for Chainlink.Request;
 
     /**
@@ -18,34 +20,38 @@ contract NFTMarket is ChainlinkClient {
      *
      */
 
-    address private owner;
+    // address private owner;
     uint256 private serviceFee = 100;     // 100 basis points == 1% (means 100 / 1000)
-    bytes32 private jobId;
-    uint256 private fee;
-    uint256 public prices;
     uint256 private collectedServiceFee = 0;
-
+    uint256 private fee;
+    bytes32 private jobId;
+    address private admin;
+    address private linkFaucetAddress = 0x326C977E6efc84E512bB9C30f76E30c160eD06FB;
+    string private backendAPI = "https://mocki.io/v1/1a4747ea-b5c1-4a11-8cb3-c841062a5585";
     bool private locked;
 
+    uint256 public prices;  // will be retrived in wei
+
     modifier noReentrant() {
-        require(!locked, "No re-entrancy");
+        require(!locked, "No re-entrancy!");
         locked = true;
         _;
         locked = false;
     }
 
-    constructor() {
-        prices = 0;
-        owner = msg.sender;
-        jobId = "ca98366cc7314957b8c012c72f05aeeb";
-        fee = (1 * LINK_DIVISIBILITY) / 10;  // varies as per network & job
-        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-        setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);  // for goerli (https://docs.chain.link/any-api/testnet-oracles)
+    modifier onlyAdmin() {
+        require(admin == msg.sender, "No right to access!");
+        _;
     }
 
-    modifier onlyOnwer() {
-        require(msg.sender == owner, "Rights reserved for the owner only!");
-        _;
+    constructor() ConfirmedOwner(msg.sender) {
+        prices = 0;
+        admin = msg.sender;
+
+        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
+        setChainlinkOracle(0xCC79157eb46F5624204f47AB42b3906cAA40eaB7);
+        jobId = "ca98366cc7314957b8c012c72f05aeeb";
+        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)  // for goerli (https://docs.chain.link/any-api/testnet-oracles)
     }
 
     function requestTokenPrice(address _collection, uint256 _tokenId) public returns (bytes32 reqestId) {
@@ -55,12 +61,19 @@ contract NFTMarket is ChainlinkClient {
             this.fulfill.selector
         );
 
-        // Set the URL to perform the GET request on
-        req.add("get","https://mocki.io/v1/e1a5efac-8e18-4f70-9947-105fa4edf466");
-        // req.addBytes("address", abi.encode(_collection)); 
-        // say; {"contract" : { "tokens" : { "value": 123} } } then req.add("path", "contract,tokens,value")
+        string memory link = string(abi.encodePacked(backendAPI, "?address=", Strings.toHexString(uint256(uint160(_collection)), 20), "&tokenId=", Strings.toString(_tokenId)));
+
+        // Set the URL to perform the GET request on "abc" + route
+        req.add(
+            "get",
+            link
+        );
         // json structure { "value": 123 }
         req.add("path", "value");
+
+        // Multiply the result by 1000000000000000000 to remove decimals
+        int256 timesAmount = 10 ** 18;
+        req.addInt("times", timesAmount);
 
         // Sends the request
         return sendChainlinkRequest(req, fee);
@@ -68,14 +81,14 @@ contract NFTMarket is ChainlinkClient {
 
     function fulfill(
         bytes32 _requestId,
-        uint256 _price
+        uint256 _volume
     ) public recordChainlinkFulfillment(_requestId) {
-        prices = _price;
+        prices = _volume;
     }
 
-    function swapOut(address _token, address _recipient, uint256 _tokenId) external payable noReentrant {
+    function swapOut(address _token, address _recipient, uint256 _tokenId) external payable noReentrant returns (bool swapStatus) {
         require(_token != address(0x0) && _recipient != address(0x0), "Token & recipient addresses connot be Zero!");
-        // requestTokenPrice(_token, _tokenId);
+        requestTokenPrice(_token, _tokenId);
         require(prices > 0, "Invalid token ID or collection");
 
         uint256 value = msg.value;  // should be in ethers
@@ -83,21 +96,20 @@ contract NFTMarket is ChainlinkClient {
         collectedServiceFee += totalFee;
         require(value >= (prices + totalFee), "Insufficient ether provided!");
 
-        IERC721(_token).safeTransferFrom(msg.sender, _recipient, _tokenId);
-
-        // send any extra ether to the target address & token price to owner
-        uint256 excessEth = value - (prices + totalFee);
-        (bool sent, ) = _recipient.call{value: excessEth}("");
-        require(sent, "Failed to send excess Ether to recipent!");
-        (sent, ) = _recipient.call{value: prices}("");
+        // send NFT price to the owner of NFT
+        (bool sent, ) = (IERC721(_token).ownerOf(_tokenId)).call{value: prices}("");
         require(sent, "Failed to send NFT price Ether to owner!");
+        
+        // send any extra ether to the target address
+        uint256 excessEth = value - (prices + totalFee);
+        (sent, ) = (msg.sender).call{value: excessEth}("");
+        require(sent, "Failed to send excess Ether to recipent!");
+
+        IERC721(_token).safeTransferFrom(msg.sender, _recipient, _tokenId);
+        swapStatus = true;
     }
 
-    function changeOwner(address _owner) external onlyOnwer {
-        owner = _owner;
-    }
-
-    function setServiceFee(uint256 basisPoints) external onlyOnwer {
+    function setServiceFee(uint256 basisPoints) external {
         serviceFee = basisPoints;
     }
 
@@ -105,11 +117,25 @@ contract NFTMarket is ChainlinkClient {
         serviceCharges = serviceFee;
     }
 
-    function claimServiceFee() external payable onlyOnwer noReentrant {
+    function changeOwner(address _owner) external  onlyAdmin {
+        require(_owner != address(0x0), "Address cannot be null!");
+        admin = _owner;
+    }
+
+    function claimServiceFee() external payable onlyAdmin noReentrant {
         assert(collectedServiceFee > 0);
-        (bool sent, ) = owner.call{value: collectedServiceFee}("");
+        (bool sent, ) = admin.call{value: collectedServiceFee}("");
         require(sent, "Failed to send service fees to owner!");
         collectedServiceFee = 0;
+    }
+
+    function claimChainLinkTokens() external onlyAdmin noReentrant {
+        assert(IERC20(linkFaucetAddress).balanceOf(address(this)) > 0);
+        IERC20(linkFaucetAddress).transfer(admin, IERC20(linkFaucetAddress).balanceOf(address(this)));
+    }
+
+    function chainLinkBalance() external view returns (uint256 _tokens) {
+        return IERC20(linkFaucetAddress).balanceOf(address(this));
     }
 
 }
--- contracts/test.sol
+++ contracts/test.sol
@@ -3,6 +3,7 @@ pragma solidity ^0.8.7;
 
 import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
 /**
  * Request testnet LINK and ETH here: https://faucets.chain.link/
@@ -54,7 +55,7 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
         // Set the URL to perform the GET request on
         req.add(
             "get",
-            "https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD"
+            "https://mocki.io/v1/e1a5efac-8e18-4f70-9947-105fa4edf466"
         );
 
         // Set the path to find the desired data in the API response, where the response format is:
@@ -68,7 +69,7 @@ contract APIConsumer is ChainlinkClient, ConfirmedOwner {
         //   }
         //  }
         // request.add("path", "RAW.ETH.USD.VOLUME24HOUR"); // Chainlink nodes prior to 1.0.0 support this format
-        req.add("path", "RAW,ETH,USD,VOLUME24HOUR"); // Chainlink nodes 1.0.0 and later support this format
+        req.add("path", "value"); // Chainlink nodes 1.0.0 and later support this format
 
         // Multiply the result by 1000000000000000000 to remove decimals
         int256 timesAmount = 10 ** 18;
